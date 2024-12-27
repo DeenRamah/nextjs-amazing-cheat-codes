849d481e58117b163d37743899505a96737c15e5
Kiprotich Kipchumba <96067114+DeenRamah@users.noreply.github.com>
2024-12-27 09:44:22 +0300

    Update createuser.js

--- createuser.js
+++ createuser.js
@@ -1,7 +1,8 @@
 //using noje js at the backend creating a new user
 
 import express from 'express'
+import { createUser } from "../controllers/userCntrl"; 
 
 const router = express.Router()
 
-router.post("/register, createUser")
+router.post("/register", createUser);
