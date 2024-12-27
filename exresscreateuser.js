5473961e15c085cd57ff8e0f82bfde86e292db3a
Kiprotich Kipchumba <96067114+DeenRamah@users.noreply.github.com>
2024-12-27 09:42:40 +0300

    Create exresscreateuser.js

---
+++ exresscreateuser.js
@@ -0,0 +1,13 @@
+//the main file to create user after being imported from the other file
+
+import asyncHandler from 'express-async-handler'
+
+import {prisma} from "../config/prosmaConfig.js"
+
+export const createUser = asyncHandler(async(req, res) =>{
+  console.log("Create a user");
+
+  let {email} = req.body;
+
+  console.log(email);
+});
